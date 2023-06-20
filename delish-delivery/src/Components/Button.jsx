612fa0bf819abbf7304657c04e3f297b2634def5
loarden <biacsi.ors@gmail.com>
2023-06-20 10:37:46 +0200

    button color when it's disabled

--- delish-delivery/src/Components/Button.jsx
+++ delish-delivery/src/Components/Button.jsx
@@ -1,12 +1,18 @@
 import { motion } from "framer-motion"
 
 function Button({ text, isSent }) {
+
+  const colorsVariants = {
+    amber: 'bg-amber-500',
+    gray: 'bg-gray-500'
+  }
+
   return (
     <motion.button
       disabled={isSent ? true : false}
       whileHover={{ scale: 0.9 }}
       whileTap={{ scale: 1.1 }}
-      className={`p-3 justify-self-center bg-amber-500 text-white rounded-md`}>
+      className={`p-3 justify-self-center ${isSent ? colorsVariants.gray : colorsVariants.amber} text-white rounded-md`}>
       {text}
     </motion.button>
   )
